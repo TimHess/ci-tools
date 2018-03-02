466f00b0f828964e5c60c4494d89ebc39586bbb2
Tim Hess <thess@pivotal.io>
2018-03-02 09:41:57 -0600

    count test errors and throw if > 0

--- scripts/appveyor_tests.ps1
+++ scripts/appveyor_tests.ps1
@@ -1,11 +1,19 @@
 Set-Location test
 
+[int]$errors = 0
+
 # run tests in each project in the test folder
 Get-ChildItem -Directory | ForEach-Object {
 	Set-Location $_.Name
 	dotnet restore
 	dotnet xunit -verbose
+	$errors = $errors + $lastexitcode
 	Set-Location ..
 }
 
 Set-Location ..
+
+If ($errors -gt 0)
+{
+    Throw "$errors tests have failed" 
+}
